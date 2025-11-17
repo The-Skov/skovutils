48d84e87d9265cf537ceb36be32d4e124eeb782d
Mads Neumers Skov <madsnskov@gmail.com>
2025-11-17 15:16:52 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ __author__ = "Mads Skov"
 __version__ = "0.1.0"
 
 # -- Imports  to expose at package level --
-from .tree import tree_to_distance_matrix, cluster_tree, lowest_common_taxa
-from .rm_correlation import rmcorr_stats
-from .filtering import minmin
+from .tree import *
+from .rm_correlation import *
+from .filtering import *
 
